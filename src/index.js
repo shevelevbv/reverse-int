97aac8046e0c7fe6e9103582a44724cbc68d3480
Bronislav Shevelev <shevelevbv@hotmail.com>
2022-01-03 22:26:15 -0500

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+  let stringNumber = n.toString();
+  let finalString = '';
+  for (let i = stringNumber.length - 1; i >= 0; i--) {
+    finalString += stringNumber[i];
+  }
+  return parseInt(finalString);
 }
